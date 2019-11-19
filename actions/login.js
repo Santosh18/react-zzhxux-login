a41753dac87ad6e1772b5f64e461713ddb91dc1d
Santosh18 <santosh.swami93@gmail.com>
2019-11-19 16:03:29 +0530

    1 file modified, 3 added

--- actions/login.js
+++ actions/login.js
@@ -12,7 +12,7 @@ export const login = (credentials) => async (dispatch, getState) => {
       email: credentials.username,
       password: credentials.password
     }
-    console.log('params ',params);
+    console.log('params ', params);
     var response = await axios.post(`http://localhost:4200/api/login?email=${params.email}&password=${params.password}`);
     console.log('res', response.data.data.token);
     localStorage.setItem('authToken', `Bearer ${response.data.data.token}`);
